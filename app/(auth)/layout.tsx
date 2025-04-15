3459aa917266393f04326188c9ddaa1279ff478d
venumadhav17 <kalyanamvenumadhav@gmail.com>
2025-04-15 15:44:53 +0530

    added group routing, layout, parent, global routing pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export default function NavbarLayout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <div>Header</div>
+      {children}
+      <div>Footer</div>
+    </div>
+  );
+}
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,3 @@
+export default function signUp() {
+  return <div>Sign Up</div>;
+}
